1f36cc447ee5f7105238282cfc2040cb6ab08c81
Jaishree <jaishreeofficial29@gmail.com>
2024-11-28 21:45:32 +0530

    Changed App Name + Removed few files

--- lib/controllers/details_controller.dart
+++
@@ -1,9 +0,0 @@
-import '../models/movie_model.dart';
-
-class DetailsController {
-  Movie? selectedMovie;
-
-  void setMovie(Movie movie) {
-    selectedMovie = movie;
-  }
-}
--- lib/views/home/movie_card.dart
+++ lib/views/home/movie_card.dart
@@ -69,6 +69,7 @@ class MovieCard extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(width: 10,),
           ],
         ),
       ),
